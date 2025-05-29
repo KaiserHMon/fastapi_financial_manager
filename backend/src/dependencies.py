ad0bad725138d6959941c4d89e35057bc76124b8
KaiserHMon <secondhardoy@gmail.com>
2025-05-29 10:59:23 -0300

    Dependencies added

---
+++ backend/src/dependencies.py
@@ -0,0 +1,16 @@
+from typing import Generator, AsyncGenerator
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from config.database import SessionLocal, AsyncSessionLocal
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
