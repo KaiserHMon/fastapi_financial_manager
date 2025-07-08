fb0286e96dfb215ae25286b25e9108f547c09560
KaiserHMon <secondhardoy@gmail.com>
2025-07-08 19:37:08 -0300

    alembic migrations

--- src/models/incomes_model.py
+++ src/models/incomes_model.py
@@ -4,7 +4,7 @@ from src.config.database import base
 
 class IncomeModel(base):
     __tablename__ = 'incomes'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     amount: Mapped[int] = mapped_column(Integer, nullable=False)
     description: Mapped[str] = mapped_column(String(255), nullable=True)
     date: Mapped[int] = mapped_column(DateTime, nullable=False)
--- src/models/user_model.py
+++ src/models/user_model.py
@@ -5,7 +5,7 @@ from src.config.database import base
 class UserModel(base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     full_name: Mapped[str] = mapped_column(String(255), nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
