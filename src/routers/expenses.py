26c4ea50ee5d7c54543476f6094f22972eef316f
KaiserHMon <secondhardoy@gmail.com>
2025-07-31 21:27:35 -0300

    expenses_services update

--- src/routers/expenses.py
+++ src/routers/expenses.py
@@ -5,13 +5,14 @@ from schemas.expenses_schema import ExpenseIn, ExpenseOut
 from models.user_model import UserModel
 from dependencies import get_async_db
 from services.auth_services import auth_access_token
-from services.expense_services import (
+from services.expenses_service import (
     create_expense as create_expense_service,
     get_expenses as get_expenses_service,
     get_expense_by_id as get_expense_by_id_service,
     update_expense as update_expense_service,
     delete_expense as delete_expense_service
 )
+
 from exceptions.http_errors import (
     EXPENSE_NOT_FOUND,
     EXPENSE_CREATION_FAILED,
---
+++ src/services/expenses_service.py
@@ -0,0 +1,44 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from models.user_model import UserModel
+from models.expenses_model import ExpenseModel
+from schemas.expenses_schema import ExpenseIn
+
+
+async def create_expense(expense: ExpenseIn, user: UserModel, db: AsyncSession) -> ExpenseModel:
+    expenses_db = ExpenseModel(**expense.model_dump(), user_id=user.id)
+    db.add(expenses_db)
+    await db.commit()
+    await db.refresh(expenses_db)
+    return expenses_db
+
+
+async def get_expenses(user: UserModel, db: AsyncSession):
+    result = await db.execute(select(ExpenseModel).where(ExpenseModel.user_id == user.id))
+    return result.scalars().all()
+
+
+async def get_expense_by_id(expense_id: int, user: UserModel, db: AsyncSession):
+    result = await db.execute(select(ExpenseModel).where(ExpenseModel.id == expense_id, ExpenseModel.user_id == user.id))
+    return result.scalars().first()
+
+
+async def update_expense(expense_id: int, expense_in: ExpenseIn, user: UserModel, db: AsyncSession):
+    expense_db = await get_expense_by_id(expense_id, user, db)
+    if not expense_db:
+        return None
+    for key, value in expense_in.model_dump().items():
+        setattr(expense_db, key, value)
+    await db.commit()
+    await db.refresh(expense_db)
+    return expense_db
+
+
+async def delete_expense(expense_id: int, user: UserModel, db: AsyncSession):
+    expense_db = await get_expense_by_id(expense_id, user, db)
+    if not expense_db:
+        return None
+    await db.delete(expense_db)
+    await db.commit()
+    return expense_db
